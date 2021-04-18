56a17836d3cc351f7a261e170b8e7403cced91ae
Viktor Tar <viktor.tar@weborigo.eu>
2021-04-18 21:11:50 +0200

    git pull

--- snap.sh
+++ snap.sh
@@ -1,3 +1,4 @@
+git pull
 npx snap https://travlrd.webflow.io/ --output static
 grep -rl '</head>' static | xargs sed -i 's|</head>|<style>.w-webflow-badge{display:none !important;}</style></head>|g'
 git add .
